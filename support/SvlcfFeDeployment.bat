a34cdce97108706cfb4eaf227f3881f313ed52f9
Thej <mukundathej@gmail.com>
2025-03-25 21:25:31 +0530

    Update SvlcfFeDeployment.bat

--- support/SvlcfFeDeployment.bat
+++ support/SvlcfFeDeployment.bat
@@ -5,7 +5,7 @@ docker rmi svlcffe
 timeout /t 2
 docker build -t svlcffe .
 timeout /t 5
-docker run --network svlcf-net --name svlcffe -e TZ=Asia/Kolkata -p 9002:9002 -v C:\Data\OneDrive:/home/Data -d svlcffe:latest
+docker run --network svlcf-net --name svlcffe -e TZ=Asia/Kolkata -p 9000:9000 -v C:\Data\OneDrive:/home/Data -d svlcffe:latest
 timeout /t 10
 cd C:/Data/Code/svlcf-be
 call mvn clean install
